6b396ef750a1ad0f419a3c7b0045953ad82aafed
H1NGx <3187511419@qq.com>
2026-06-19 15:34:49 +0800

    Add sundial exhibit click navigation

---
+++ Assets/scripts/ClickSceneLoader.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ClickSceneLoader : MonoBehaviour
+{
+    [SerializeField] private string sceneName = "Standard Demo";
+
+    private bool isLoading;
+
+    private void OnMouseDown()
+    {
+        LoadTargetScene();
+    }
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0) || IsPointerOverUi())
+        {
+            return;
+        }
+
+        Camera camera = Camera.main != null ? Camera.main : FindObjectOfType<Camera>();
+        if (camera == null)
+        {
+            return;
+        }
+
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, Physics.DefaultRaycastLayers, QueryTriggerInteraction.Collide)
+            && hit.collider != null
+            && hit.collider.gameObject == gameObject)
+        {
+            LoadTargetScene();
+        }
+    }
+
+    private void LoadTargetScene()
+    {
+        if (isLoading)
+        {
+            return;
+        }
+
+        if (!Application.CanStreamedLevelBeLoaded(sceneName))
+        {
+            Debug.LogError($"Scene '{sceneName}' is not enabled in Build Settings.");
+            return;
+        }
+
+        isLoading = true;
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private static bool IsPointerOverUi()
+    {
+        return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject();
+    }
+}
